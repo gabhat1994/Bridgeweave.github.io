200ce5c8c696dae7cdde404f302a39ead649174c
gabhat-NG <106967882+gabhat-NG@users.noreply.github.com>
2023-12-05 20:28:03 +0530

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Counter } from './Counter'
+import { Route, Routes } from 'react-router-dom'
 import About from './pages/About'
 import Home from './pages/Home'
 
 export const App = () => {
   return (
-    <BrowserRouter>
     <Routes>
       <Route path='/'  element={<Home />} />
       <Route path='/details' element={<About />} />
     </Routes>
-    </BrowserRouter>
   )
 }
