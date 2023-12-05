ce4306ba24043675942f9f2436d72857c859b52f
gabhat-NG <106967882+gabhat-NG@users.noreply.github.com>
2023-12-05 19:55:55 +0530

    add component for routes

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface AboutProps {
+    
+}
+ 
+const About: FunctionComponent<AboutProps> = (props) => {
+    return (<div><p>This is about page</p></div>  );
+}
+ 
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from "react";
+
+interface HomeProps {
+    
+}
+ 
+const Home: FunctionComponent<HomeProps> = (props) => {
+    return (<div><p>This is home page</p></div>  );
+}
+ 
+export default Home;
