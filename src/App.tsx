6b4a420b15c689b599053d37cb83d04687332ce1
gabhat-NG <106967882+gabhat-NG@users.noreply.github.com>
2023-12-05 19:27:02 +0530

    Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { Counter } from './Counter'
+
 export const App = () => {
-    return <h1>React Typescript Starter Template</h1>
+  return (
+    <>
+      <h1>React TypeScript Webpack Starter Template</h1>
+      <Counter />
+    </>
+  )
 }
